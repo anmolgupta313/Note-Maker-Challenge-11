2540766c69138b7384225cd8fe9858bdba9b6057
Anmol Gupta <anmol.gupta2180@gmail.com>
2022-11-16 20:59:13 -0500

    updating

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Note-Maker-Challenge-11
+
+# Description Of the Challenge
+This is the 11th challenge that were given to us by the University Of Toronto in the coding bootcamp. In this challenge we have been asked to complete the code which purpose is to add get and post notes data into a file and and point a file to particular point using nodejs and js, express.
+
+# Completed
+by following all the instructions I am able to done this task and in this task I have completed the code by writing the js and by installing the node library.
+
+# Result
+![Screenshot 2022-11-16 at 5 50 51 PM](https://user-images.githubusercontent.com/111723339/202312011-ea8bc621-9ef5-4f98-a4c1-51950c077628.png)
+
+Demonstration:
+https://drive.google.com/file/d/1qKZ1-xk3QfwGwR1CD5Hi7ljCkfIibCeJ/view
+
+Repository Links
+https://github.com/anmolgupta313/Note-Maker-Challenge-11/issues/new
+
+License
+Please refer to the LICENSE in the repo.
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express= require("express");
-const path= require('path');
-const uuid= require('uuid');
-const db= require('./db/db.json');
-const fs= require('fs');
-const PORT= 3001;
+const express = require("express");
+const path = require('path');
+const uuid = require('uuid');
+const db = require('./db/db.json');
+const fs = require('fs');
+const PORT = process.env.PORT || 3001;
 
-const app= express();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,21 +13,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
-app.get('/', (req, res) =>{
-  res.sendFile(path.join(__dirname, '/public/index.html'))}
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+}
 );
 
-app.get('/notes', (req, res) =>{
-  res.sendFile(path.join(__dirname, '/public/notes.html'))}
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+}
 );
 
-app.get('/api/notes', (req,res)=>{
+app.get('/api/notes', (req, res) => {
   res.json(db);
 })
 
 
-app.post('/api/notes',(req,res)=>{
-  let data= {
+app.post('/api/notes', (req, res) => {
+  let data = {
     id: uuid.v4(),
     title: req.body.title,
     text: req.body.text
@@ -37,18 +39,34 @@ app.post('/api/notes',(req,res)=>{
 
   res.json(db);
   fs.writeFile('./db/db.json', JSON.stringify(db),
-  (err,text)=>{
-    if(err){
-      console.log(err);
-      return
-    }
-    console.log("Success");
-  });
+    (err, text) => {
+      if (err) {
+        console.log(err);
+        return
+      }
+      console.log("Success");
+    });
 })
 
+// app.delete("/api/notes/:id", function(req, res) {
+//   let savedNotes = db;
+//   let noteID = req.params.id;
+//   let newID = 0;
+//   console.log(`Deleting note with ID ${noteID}`);
+//   savedNotes = savedNotes.filter(currNote => {
+//       return currNote.id != noteID;
+//   })
+  
+//   for (currNote of savedNotes) {
+//       currNote.id = newID.toString();
+//       newID++;
+//   }
 
+//   fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+//   res.json(savedNotes);
+// })
 
-app.listen(PORT,()=>{
-    console.log(`http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
 })
 
